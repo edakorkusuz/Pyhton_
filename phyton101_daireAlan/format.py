e1a792a6a2c2ffc781107a0a7c982f50280be826
Eda Nur Korkusuz <korkusuzeda2706@gmail.com>
2024-03-14 21:58:43 +0300

    String diziler üzerinde calisilsi

---
+++ phyton101_daireAlan/format.py
@@ -0,0 +1,14 @@
+name='Eda'
+surname= 'Korkusuz'
+
+age=22
+'''
+print('My name is {}'.format(name))
+print ('My name is {} My lastname is {}'.format(name,surname)) # formatı . ile çağırıyoruz
+print("My name is {1} my lastname is {0}".format(surname, name)) # index numaralarını bu şekilde verebiliriz
+print ("My name is {} {}. I'm {} years old".format(name, surname,age))'''
+ 
+res= 200 / 700
+
+
+print( 'deger: {r:1.5}'.format(r=res))#virgülden sonra 5 basamak yazdir
---
+++ phyton101_daireAlan/stringdemo.py
@@ -0,0 +1,16 @@
+website='wwww.ktu.edu.tr'
+course='Python Kursu: Bastan Sona Pyhton Programlama Rehberiniz (40 saat)'
+
+
+#'course' karakter dizisinde kaç karakter bulunmaktadır
+
+print(len(course))
+
+#'website içinde www karakterini alın'
+
+print(website[0:3])
+
+#'course' içinde ilk 15 ve son 15 karakteri alın
+
+print (course[0:15]+ ''+ course[-15:-1])
+
